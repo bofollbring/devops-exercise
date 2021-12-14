4f91f2d65f69471350690d8db07301ebe6089595
bofollbring <bo.follbring@gmail.com>
2021-12-14 17:17:17 +1000

    Update build.tf

--- deployment/build.tf
+++ deployment/build.tf
@@ -1,1 +1,59 @@
+esource "kubernetes_deployment" "time" {
+  metadata {
+    name = "terraform-time"
+    labels = {
+      test = "MyTimeApp"
+    }
+  }
 
+  spec {
+    replicas = 3
+
+    selector {
+      match_labels = {
+        test = "MyTimeApp"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          test = "MyTimeApp"
+        }
+      }
+
+      spec {
+        container {
+          image = "nginx:1.7.8"
+          name  = "time"
+
+          resources {
+            limits = {
+              cpu    = "0.5"
+              memory = "512Mi"
+            }
+            requests = {
+              cpu    = "250m"
+              memory = "50Mi"
+            }
+          }
+
+          liveness_probe {
+            http_get {
+              path = "/nginx_status"
+              port = 80
+
+              http_header {
+                name  = "X-Custom-Header"
+                value = "Awesome"
+              }
+            }
+
+            initial_delay_seconds = 3
+            period_seconds        = 3
+          }
+        }
+      }
+    }
+  }
+}
